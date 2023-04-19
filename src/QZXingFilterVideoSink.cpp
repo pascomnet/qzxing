84f218534acbceff6d8d0cfa2db6fc37d128ea1f
Jan Lorenz <jan.lorenz@pascom.net>
2023-04-19 17:40:10 +0200

    CL-3461 fix video frame unmapping on android

--- src/QZXingFilterVideoSink.cpp
+++ src/QZXingFilterVideoSink.cpp
@@ -55,60 +55,54 @@ int QZXingFilter::orientation() const
 void QZXingFilter::setVideoSink(QObject *videoSink){
     m_videoSink = qobject_cast<QVideoSink*>(videoSink);
 
-    connect(m_videoSink, &QVideoSink::videoFrameChanged, this, &QZXingFilter::processFrame);
+    connect(m_videoSink, &QVideoSink::videoFrameChanged, this, &QZXingFilter::processFrame, Qt::DirectConnection);
 }
 
-void QZXingFilter::processFrame(const QVideoFrame &frame) {
-#ifdef Q_OS_ANDROID
-    m_videoSink->setRhi(nullptr); // https://bugreports.qt.io/browse/QTBUG-97789
-    QVideoFrame f(frame);
-    f.map(QVideoFrame::ReadOnly);
-#else
-    const QVideoFrame &f = frame;
-#endif // Q_OS_ANDROID
-
+void QZXingFilter::processFrame(const QVideoFrame &f) {
+    // Cleaned up version of https://github.com/CMGeorge/qzxing/commit/10b917f071b1f5b0145f04bb10ec8404d314165e#diff-cccfcad0b6ba438f5e4e6219f041e86fa651ea64921b2ba25ed5703af20c879f
     if(!isDecoding() && processThread.isFinished()){
         decoding = true;
-
-        QImage image = f.toImage();
-        processThread = QtConcurrent::run([=](){
-            if(image.isNull())
-            {
-                qDebug() << "QZXingFilter error: Cant create image file to process.";
-                decoding = false;
-                return;
-            }
-
-            QImage frameToProcess(image);
-            const QRect& rect = captureRect.toRect();
-
-            if (captureRect.isValid() && frameToProcess.size() != rect.size()) {
-                frameToProcess = image.copy(rect);
-            }
-
-            if (!orientation_) {
-                decoder.decodeImage(frameToProcess);
-            } else {
-                QTransform transformation;
-                transformation.translate(frameToProcess.rect().center().x(), frameToProcess.rect().center().y());
-                transformation.rotate(-orientation_);
-
-                QImage translatedImage = frameToProcess.transformed(transformation);
-
-                decoder.decodeImage(translatedImage);
-            }
-
-            //        static int i=0;
-            //        qDebug() << "image.size()" << frameToProcess.size();
-            //        qDebug() << "image.format()" << frameToProcess.format();
-            //        const QString path = QStandardPaths::writableLocation(QStandardPaths::PicturesLocation) + "/qrtest/test_" + QString::number(i++ % 100) + ".png";
-            //        qDebug() << "saving image" << i << "at:" << path << frameToProcess.save(path);
-
-            decoder.decodeImage(frameToProcess, frameToProcess.width(), frameToProcess.height());
-        });
-    }
-
+        bool didWeMapTheFrame = false;
+        QVideoFrame frame{f};
+        if (!frame.isMapped()) {
 #ifdef Q_OS_ANDROID
-    f.unmap();
+            m_videoSink->setRhi(nullptr); // https://bugreports.qt.io/browse/QTBUG-97789
 #endif
+            didWeMapTheFrame = frame.map(QVideoFrame::ReadOnly);
+        }
+
+        if (frame.isMapped()) {
+            QImage image = frame.toImage();
+            if (!image.isNull()) {
+                processThread = QtConcurrent::run([=]() {
+                    QImage frameToProcess{image};
+                    const QRect &rect = captureRect.toRect();
+
+                    if (captureRect.isValid() && frameToProcess.size() != rect.size()) {
+                        frameToProcess = image.copy(rect);
+                    }
+
+                    if (!orientation_) {
+                        decoder.decodeImage(frameToProcess);
+                    } else {
+                        QTransform transformation;
+                        transformation.translate(frameToProcess.rect().center().x(), frameToProcess.rect().center().y());
+                        transformation.rotate(-orientation_);
+                        QImage translatedImage = frameToProcess.transformed(transformation);
+                        decoder.decodeImage(translatedImage);
+                    }
+
+                    decoder.decodeImage(frameToProcess, frameToProcess.width(), frameToProcess.height());
+                    decoding = false;
+                });
+            } else {
+                decoding = false;
+            }
+            if(didWeMapTheFrame) {
+                frame.unmap();
+            }
+        } else {
+            decoding = false;
+        }
+    }
 }
